671cc99934a631bd63dfbc480f70d0ba5d401e03
koutsn <koutsn@gmail.com>
2025-11-05 23:34:05 +0100

    Login Controller

--- src/main/java/org/realEstate/myRealEstateService/controller/LoginController.java
+++ src/main/java/org/realEstate/myRealEstateService/controller/LoginController.java
@@ -1,32 +1,24 @@
 package org.realEstate.myRealEstateService.controller;
 
+import jakarta.validation.Valid;
 import org.myRealEstate.api.LoginApi;
-import org.springframework.http.HttpStatus;
+import org.myRealEstate.model.LoginUser200Response;
+import org.myRealEstate.model.LoginUserRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
-
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/login")
 public class LoginController implements LoginApi {
 
-    @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> credentials) {
-        String username = credentials.get("username");
-        String password = credentials.get("password");
+    @Override
+    @PostMapping("loginUser")
+    public ResponseEntity<LoginUser200Response> loginUser(@Valid LoginUserRequest loginUserRequest) {
 
-        if ("user".equals(username) && "pass".equals(password)) {
-            Map<String, String> body = new HashMap<>();
-            body.put("token", "mock-token-12345");
-            return ResponseEntity.ok(body);
-        }
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        LoginUser200Response loginUser200Response = new LoginUser200Response();
+        loginUser200Response.setToken("dummy-token");
+        return ResponseEntity.ok(loginUser200Response);
     }
 }
