22accab285f56076e032134a86cf1320ed98a82f
koutsn <koutsn@gmail.com>
2025-11-06 11:39:35 +0100

    Code Cleanup

--- src/main/java/org/realEstate/myRealEstateService/controller/LoginController.java
+++ src/main/java/org/realEstate/myRealEstateService/controller/LoginController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.myRealEstate.api.LoginApi;
 import org.myRealEstate.model.LoginUser200Response;
 import org.myRealEstate.model.LoginUserRequest;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
