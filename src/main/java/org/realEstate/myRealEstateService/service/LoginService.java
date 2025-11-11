6f390635550f8f1f1bf8012ca3781e2289392bfb
koutsn <koutsn@gmail.com>
2025-11-11 12:20:22 +0100

    Add superuser option

--- src/main/java/org/realEstate/myRealEstateService/service/LoginService.java
+++ src/main/java/org/realEstate/myRealEstateService/service/LoginService.java
@@ -7,6 +7,7 @@ import org.realEstate.myRealEstateService.exception.UnauthorizedException;
 import org.realEstate.myRealEstateService.repository.UserRepository;
 import org.realEstate.myRealEstateService.utils.Encrypt;
 import org.realEstate.myRealEstateService.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,17 +16,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class LoginService {
 
+    @Value("${super.password}")
+    private String userPass;
+
     private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
 
     public String login(String username, String password) throws UnauthorizedException {
 
+        if ("super".equals(username) && userPass.equals(password)) {
+            String token = jwtUtil.generateToken(username, "ADMIN");
+            return token;
+        }
 
         UserEntity user = userRepository.findByUsernameAndPassword(username, Encrypt.encryptPassword(password)).orElse(null);
-
         if (user != null && user.getUsername() != null && user.getRole() != null) {
-            String token = jwtUtil.generateToken(user.getUsername(),user.getRole());
-            return token;
+            return jwtUtil.generateToken(user.getUsername(), user.getRole());
         }
         throw new UnauthorizedException("Invliad username or password");
     }
