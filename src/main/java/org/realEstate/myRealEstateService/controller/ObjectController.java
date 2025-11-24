ffd4641ae238df54ad73665a8f9d22ec8af829be
koutsn <koutsn@gmail.com>
2025-11-24 19:55:02 +0100

    Object Controller

--- src/main/java/org/realEstate/myRealEstateService/controller/ObjectController.java
+++ src/main/java/org/realEstate/myRealEstateService/controller/ObjectController.java
@@ -1,63 +1,30 @@
 package org.realEstate.myRealEstateService.controller;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.RequiredArgsConstructor;
-import org.realEstate.myRealEstateService.dto.ObjecFilesDto;
+import org.realEstate.myRealEstateService.dto.ObjectDto;
 import org.realEstate.myRealEstateService.response.ErrorResponse;
-import org.realEstate.myRealEstateService.service.ObjectFileService;
+import org.realEstate.myRealEstateService.service.ObjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.UUID;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${spring.application.name}")
 public class ObjectController {
-
-    private final ObjectFileService objectFileService;
+    private final ObjectService objectService;
 
     @PreAuthorize("hasAuthority('USER')")
     @PostMapping(
-            value = "/object/{id}/images",
+            value = "/object",
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE
     )
-    public ResponseEntity<?> createImage(@PathVariable UUID id, @ModelAttribute ObjecFilesDto file) {
+    public ResponseEntity<?> createObject(@ModelAttribute ObjectDto object) {
         try {
-            objectFileService.uploadImages(id, file);
+            objectService.createObject(object);
             return ResponseEntity.created(null).build();
-        } catch (Exception e) {
-            ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(response);
-        }
-    }
-
-    @GetMapping("/object/{id}/images")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    public ResponseEntity<?> getObjectImages(@PathVariable UUID id) {
-        try {
-            List<ObjecFilesDto> objectImages = objectFileService.getImagesForObject(id);
-            return ResponseEntity.ok(objectImages);
-        } catch (Exception e) {
-            ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(response);
-        }
-    }
-
-    @GetMapping("/image/{id}")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    public ResponseEntity<?> getImage(@PathVariable UUID id) {
-        try {
-            ObjecFilesDto objectImage = objectFileService.getImageById(id);
-            return ResponseEntity.ok(objectImage);
         } catch (Exception e) {
             ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
             return ResponseEntity
---
+++ src/main/java/org/realEstate/myRealEstateService/controller/ObjectFileController.java
@@ -0,0 +1,68 @@
+package org.realEstate.myRealEstateService.controller;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.RequiredArgsConstructor;
+import org.realEstate.myRealEstateService.dto.ObjecFilesDto;
+import org.realEstate.myRealEstateService.response.ErrorResponse;
+import org.realEstate.myRealEstateService.service.ObjectFileService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${spring.application.name}")
+public class ObjectFileController {
+
+    private final ObjectFileService objectFileService;
+
+    @PreAuthorize("hasAuthority('USER')")
+    @PostMapping(
+            value = "/object/{id}/images",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<?> createImage(@PathVariable UUID id, @ModelAttribute ObjecFilesDto file) {
+        try {
+            objectFileService.uploadImages(id, file);
+            return ResponseEntity.created(null).build();
+        } catch (Exception e) {
+            ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+    }
+
+    @GetMapping("/object/{id}/images")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public ResponseEntity<?> getObjectImages(@PathVariable UUID id) {
+        try {
+            List<ObjecFilesDto> objectImages = objectFileService.getImagesForObject(id);
+            return ResponseEntity.ok(objectImages);
+        } catch (Exception e) {
+            ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+    }
+
+    @GetMapping("/image/{id}")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public ResponseEntity<?> getImage(@PathVariable UUID id) {
+        try {
+            ObjecFilesDto objectImage = objectFileService.getImageById(id);
+            return ResponseEntity.ok(objectImage);
+        } catch (Exception e) {
+            ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+    }
+}
