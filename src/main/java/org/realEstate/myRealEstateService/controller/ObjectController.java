52a1d1e75b888c9c9c9345def18e13727ea750d4
koutsn <koutsn@gmail.com>
2025-11-25 12:28:21 +0100

    Object controller

--- src/main/java/org/realEstate/myRealEstateService/controller/ObjectController.java
+++ src/main/java/org/realEstate/myRealEstateService/controller/ObjectController.java
@@ -10,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${spring.application.name}")
@@ -18,13 +21,57 @@ public class ObjectController {
 
     @PreAuthorize("hasAuthority('USER')")
     @PostMapping(
-            value = "/object",
-            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+            value = "/object"
     )
     public ResponseEntity<?> createObject(@ModelAttribute ObjectDto object) {
         try {
             objectService.createObject(object);
             return ResponseEntity.created(null).build();
+        } catch (Exception e) {
+            ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+    }
+
+    @GetMapping(
+            value = "/objects"
+    )
+    public ResponseEntity<?> getAllObjects() {
+        try {
+            List<ObjectDto> objects = objectService.getAllObjects();
+            return ResponseEntity.ok(objects);
+        } catch (Exception e) {
+            ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+    }
+
+    @GetMapping(
+            value = "/object/|{id}"
+    )
+    public ResponseEntity<?> getAllObjects(@PathVariable UUID id) {
+        try {
+            ObjectDto objects = objectService.getObjectById(id);
+            return ResponseEntity.ok(objects);
+        } catch (Exception e) {
+            ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+    }
+
+    @DeleteMapping(
+            value = "/object/|{id}"
+    )
+    public ResponseEntity<?> deleteObject(@PathVariable UUID id) {
+        try {
+            objectService.deleteObject(id);
+            return ResponseEntity.noContent().build();
         } catch (Exception e) {
             ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
             return ResponseEntity
